210c544114c4215d633060186db7e18e03f4aa0a
肖金 <xiaojin1992@126.com>
2019-08-07 14:41:50 +0800

    dream it

--- src/open-explorer.ts
+++ src/open-explorer.ts
@@ -6,7 +6,7 @@ export default async (path: string) => {
     const asyncExec = util.promisify(exec);
     // possible outcomes -> 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
     const isWin = /^win/.test(process.platform);
-    if (!isWin) {
+    if (isWin) {
         return await asyncExec(`Explorer.exe /select,"${path}"`);
     } else {
         // return await Promise.reject('暂时只支持windows系统，见谅！'); TODO
